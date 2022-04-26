183d90568529ac453f95af9f3b0c352eaa511c00
Paula Rodriguez <Papita@Jozsefs-MacBook-Pro.local>
2022-04-26 20:52:15 +0100

    images

--- js/intermediate.js
+++ js/intermediate.js
@@ -9,7 +9,7 @@
         c: "Soup",
       },
       correctAnswer: "a", 
-      img: "/Images/Food/frenchtoast.jpeg",
+      img: "./Images/food/frenchtoast.jpeg",
     },
 
     {
@@ -21,7 +21,7 @@
         c: "Focaccia",
       },
       correctAnswer: "a", 
-      img: "/Images/Food/bruscheta.jpeg",
+      img: "./Images/food/bruscheta.jpeg",
     },
 
     {
@@ -33,7 +33,7 @@
         c: "Mexican sauce",
       },
       correctAnswer: "b", 
-      img: "/Images/Food/guacamole.jpeg",
+      img: "./Images/food/guacamole.jpeg",
     },
   ];
 
